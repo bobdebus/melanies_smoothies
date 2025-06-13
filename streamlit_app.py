6d2a096ce80ddbcd7d2b84392984705f8724d959
Bob Debus <116631196+bobdebus@users.noreply.github.com>
2025-06-13 14:52:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,7 +10,9 @@ st.write(
   """
 )
 
-session = get_active_session()
+# session = get_active_session()
+cnx = st.connetion("Snowflake")
+session = cnx.session()
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
